fd85bfc1185cddf2f7a4f367db5b05d17ac24e5b
Asrorbek Shahobiddinov <asror797shahobiddinov@gmail.com>
2022-11-02 19:01:38 +0500

    users entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,8 +1,52 @@
-import { Entity , Column , PrimaryGeneratedColumn } from "typeorm";
+import { Entity , Column , PrimaryGeneratedColumn , CreateDateColumn , UpdateDateColumn} from "typeorm";
 
 @Entity({name:"user"})
 export class User {
    @PrimaryGeneratedColumn("uuid")
    id:string
+
+   // User Fullname Column
+   @Column({
+      type:"varchar",
+      length:128,
+      nullable:false
+   })
+   fullname:string
+
+   // User Email Column
+   @Column({
+      type:"varchar",
+      length:128,
+      nullable:false
+   })
+   emial:string
+
+   // User Password Collumn
+   @Column({
+      type:"text",
+      nullable:false
+   })
+   password:string
+
+   // User Status Column
+   @Column({
+      type:"boolean",
+      default:false
+   })
+   isVerified:Boolean
+
+   // User Data Created Date Column
+   @CreateDateColumn({
+      type:"time",
+      nullable:false
+   })
+   createdAt:string
+
+   // User Data Updated Date Column
+   @UpdateDateColumn({
+      type:"time",
+      nullable:false
+   })
+   updateAt:string
 }
 
